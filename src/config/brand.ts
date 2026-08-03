a1117c305a7d4720b5956fe30d6711f43a7006c4
ashlih84-eng <ashlih84@gmail.com>
2026-08-03 10:37:50 -0500

    branding refactor: add src/config/brand.ts

---
+++ src/config/brand.ts
@@ -0,0 +1,7 @@
+export const brandConfig={
+  productName:'Chef Cinnamon’s Reading Adventure',
+  shortName:'Chef Cinnamon',
+  worldName:'The Storybook Kingdom',
+}as const;
+
+export type BrandConfig=typeof brandConfig;
