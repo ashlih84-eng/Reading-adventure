1799c9ce3bcb063d7ac50aed96bd9de80444ccb5
ashlih84-eng <ashlih84@gmail.com>
2026-08-03 10:31:29 -0500

    map and shop increment: add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+export default tseslint.config(
+  {ignores:['dist','node_modules']},
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {files:['**/*.{ts,tsx}'],languageOptions:{globals:{...globals.browser}},rules:{'react-hooks/exhaustive-deps':'off'}},
+  {files:['public/sw.js'],languageOptions:{globals:{...globals.serviceworker}}},
+);
