30cb039d6b3f8e72bfdc6b5e3514ecb41b4b97be
ashlih84-eng <ashlih84@gmail.com>
2026-08-03 10:32:03 -0500

    map and shop increment: update src/test/setup.ts

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,5 @@
-import'@testing-library/jest-dom';
+import'@testing-library/jest-dom/vitest';
+import'fake-indexeddb/auto';
+import{cleanup}from'@testing-library/react';
+import{afterEach}from'vitest';
+afterEach(cleanup);
